8a5b3e723ab6b255874183ed44098ff690685bdf
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-16 18:02:19 -0700

    WIP - working through combination of writing sources.

--- markov.py
+++ markov.py
@@ -1,6 +1,8 @@
 """Generate Markov text from text files."""
 
 from random import choice
+import sys
+from string import punctuation
 
 
 def open_and_read_file(file_path):
@@ -10,33 +12,26 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    return open(file_path).read().replace("\n", " ").rstrip()
+    return open(file_path).read().replace("\n", " ").replace("  ", " ").rstrip()
+    # text2 = open(file_path2).read().replace("\n", " ").replace("  ", " ").rstrip()
 
 
-def make_chains(text_string):
+def make_chains(text_string, n):
     """Take input text as string; return dictionary of Markov chains.
-
-    A chain will be a key that consists of a tuple of (word1, word2)
-    and the value would be a list of the word(s) that follow those two
-    words in the input text.
-
-    For example:
-
-        >>> chains = make_chains("hi there mary hi there juanita")
-
-    Each bigram (except the last) will be a key in chains:
-
-        >>> sorted(chains.keys())
-        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
-
-    Each item in chains is a list of all possible following words:
-
-        >>> chains[('hi', 'there')]
-        ['mary', 'juanita']
-        
-        >>> chains[('there','juanita')]
-        [None]
     """
+
+    # chains = {}
+
+    # # split into list
+    # words = text_string.split(" ")
+    # # loop through range of list length
+    # for i in range(len(words) - 2):
+    #     # create tuple for list[i] and list[i+1] as dict key
+    #     bi_gram = (words[i], words[i + 1])
+    #     # chains[tuple] = chains.get(chains[tuple] + next_word, next_word)
+    #     chains[bi_gram] = chains.get(bi_gram, []) + [words[i + 2]]
+
+    # return chains
 
     chains = {}
 
@@ -44,14 +39,24 @@ def make_chains(text_string):
     words = text_string.split(" ")
 
     # loop through range of list length
-    for i in range(len(words) - 2):
+    for i in range(len(words) - n):
         # create tuple for list[i] and list[i+1] as dict key
-        bi_gram = (words[i], words[i + 1])
+        n_gram = tuple(words[i:i + n])
         # chains[tuple] = chains.get(chains[tuple] + next_word, next_word)
-        chains[bi_gram] = chains.get(bi_gram, []) + [words[i + 2]]
+        chains[n_gram] = chains.get(n_gram, []) + [words[i + n]]
 
     return chains
 
+
+def compare_two_sources(source1, source2):
+    # loop thru source1, check if keys match any keys in source2
+    for key in source1:
+        # if match, add source1[key] to source2[key]
+        # if no match, create key and add value
+        source2[key] = source2.get(key, []) + [source1[key]]
+
+    return source2
+    
 
 def make_text(chains):
     """Return text from chains."""
@@ -61,13 +66,18 @@ def make_text(chains):
     # while loop:
     while True:
         # if words == []
-        # choose random key tuple from chains & add key[0] & key[1] to words list
+        # choose random key tuple from chains & add n keys to words list
         if words == []:  # exercise! find another way to check empty list
-            current_key = choice(list(chains))
-            words.extend([current_key[0], current_key[1]])
+            # create list of keys where key[0][0] isupper() == True
+            capital_keys = [key for key in chains if key[0][0].isupper()]
 
-        # for latest two words in list (current_key)
-        current_key = (words[-2], words[-1])
+            current_key = choice(capital_keys)
+
+            n = len(current_key)
+            words.extend(current_key[:n])
+
+        # for last n words in list (current_key)
+        current_key = tuple(words[-n:])
 
         # try, except KeyError: break
         try:
@@ -79,18 +89,27 @@ def make_text(chains):
         except KeyError:
             break
 
-    return " ".join(words)
+    strung_words = " ".join(words)
+
+    # remove anything after last punctuation
+    if strung_words[-1] not in punctuation:
+
+        for i in range(len(strung_words)):
+
+            if strung_words[-i] in punctuation:
+                last_punct = -i
+                strung_words = strung_words[:last_punct + 1]
+                # print(last_punct)
+                break
+
+    return strung_words
 
 
-input_path = "green-eggs.txt"
+chain1 = make_chains(open_and_read_file(sys.argv[1]), int(sys.argv[3]))
+chain2 = make_chains(open_and_read_file(sys.argv[2]), int(sys.argv[3]))
 
-# Open the file and turn it into one long string
-input_text = open_and_read_file(input_path)
+sources_combined = compare_two_sources(chain1, chain2)
 
-# Get a Markov chain
-chains = make_chains(input_text)
+generate_text = make_text(sources_combined)
 
-# Produce random text
-random_text = make_text(chains)
-
-# print(chains)
+print(generate_text)
